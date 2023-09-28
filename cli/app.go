6d31d6fcce3e299b3622aca4422b351055373876
Alex Athanasopoulos <alex@melato.org>
2023-09-28 15:57:02 +0300

    refactoring: moved Run to cli/App

---
+++ cli/app.go
@@ -0,0 +1,65 @@
+package cli
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+
+	"melato.org/cloudconfig"
+	"melato.org/cloudconfig/local"
+	"melato.org/cloudconfig/ostype"
+)
+
+type App struct {
+	OS string
+	os cloudconfig.OSType
+}
+
+func (t *App) Configured() error {
+	switch t.OS {
+	case "":
+	case "alpine":
+		t.os = &ostype.Alpine{}
+	case "debian":
+		t.os = &ostype.Debian{}
+	default:
+		return fmt.Errorf("unrecognized OS.  accepted values are alpine, debian")
+	}
+	return nil
+}
+
+func (t *App) Apply(configFiles ...string) error {
+	configurer := cloudconfig.NewConfigurer(&local.BaseConfigurer{})
+	configurer.OS = t.os
+	configurer.Log = os.Stdout
+	if len(configFiles) == 1 && configFiles[0] == "-" {
+		return configurer.ApplyStdin()
+	} else {
+		return configurer.ApplyConfigFiles(configFiles...)
+	}
+}
+
+func (t *App) Print(file string) error {
+	config, err := cloudconfig.ReadFile(file)
+	if err != nil {
+		return err
+	}
+	data, err := cloudconfig.Marshal(config)
+	if err != nil {
+		return err
+	}
+	os.Stdout.Write(data)
+	return nil
+}
+
+func Parse(files []string) error {
+	for _, file := range files {
+		_, err := cloudconfig.ReadFile(file)
+		if err != nil {
+			fmt.Printf("%s ERROR\n", file)
+			return err
+		}
+		fmt.Printf("%s OK\n", file)
+	}
+	return nil
+}
--- main/cloudconfig.go
+++ main/cloudconfig.go
@@ -3,11 +3,8 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"os"
 
-	"melato.org/cloudconfig"
-	"melato.org/cloudconfig/local"
-	"melato.org/cloudconfig/ostype"
+	"melato.org/cloudconfig/cli"
 	"melato.org/command"
 	"melato.org/command/usage"
 )
@@ -18,66 +15,12 @@ var version string
 //go:embed usage.yaml
 var usageData []byte
 
-type Run struct {
-	OS string
-	os cloudconfig.OSType
-}
-
-func (t *Run) Configured() error {
-	switch t.OS {
-	case "":
-	case "alpine":
-		t.os = &ostype.Alpine{}
-	case "debian":
-		t.os = &ostype.Debian{}
-	default:
-		return fmt.Errorf("unrecognized OS.  accepted values are alpine, debian")
-	}
-	return nil
-}
-
-func (t *Run) Apply(configFiles ...string) error {
-	configurer := cloudconfig.NewConfigurer(&local.BaseConfigurer{})
-	configurer.OS = t.os
-	configurer.Log = os.Stdout
-	if len(configFiles) == 1 && configFiles[0] == "-" {
-		return configurer.ApplyStdin()
-	} else {
-		return configurer.ApplyConfigFiles(configFiles...)
-	}
-}
-
-func (t *Run) Print(file string) error {
-	config, err := cloudconfig.ReadFile(file)
-	if err != nil {
-		return err
-	}
-	data, err := cloudconfig.Marshal(config)
-	if err != nil {
-		return err
-	}
-	os.Stdout.Write(data)
-	return nil
-}
-
-func Parse(files []string) error {
-	for _, file := range files {
-		_, err := cloudconfig.ReadFile(file)
-		if err != nil {
-			fmt.Printf("%s ERROR\n", file)
-			return err
-		}
-		fmt.Printf("%s OK\n", file)
-	}
-	return nil
-}
-
 func main() {
 	cmd := &command.SimpleCommand{}
-	var app Run
+	var app cli.App
 	cmd.Command("apply").Flags(&app).RunFunc(app.Apply)
 	cmd.Command("print").Flags(&app).RunFunc(app.Print)
-	cmd.Command("parse").RunFunc(Parse)
+	cmd.Command("parse").RunFunc(cli.Parse)
 	cmd.Command("version").RunFunc(func() { fmt.Println(version) })
 
 	usage.Apply(cmd, usageData)
